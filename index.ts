be0d8de587c2d9802483deba70c3460856767bfc
owias tariq <165525244+1b7owais@users.noreply.github.com>
2024-07-14 12:32:33 +0500

    I am Pushing My Project

---
+++ index.ts
@@ -0,0 +1,18 @@
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random()*20+1);
+
+const answer = await inquirer.prompt([
+    {
+        name:"guessNumber",
+        message:"Guess a number bt 1-20",
+        type:"number"
+    },
+]);
+
+if (answer.guessNumber === randomNumber) {
+    console.log("congtratulation! You select a right Number");
+} else {
+    console.log("Oop! You select a wrong Number");
+    
+}
